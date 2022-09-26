20c056a82270f39564276804a73aef467b8b5bd8
schrolan <schrolan000@gmail.com>
2022-09-26 14:56:23 -0500

    I added the variables that will be the questions and answers.

---
+++ assets/script.js
@@ -0,0 +1,46 @@
+//These will be the questions
+
+var questions = [
+    {
+        question: "Do Java and JavaScript mean the same thing?"
+        answers: {
+            a: 'Why yes of course, they both have Java in them',
+            b: 'Absolutely not, if they were the same they would have the same name.',
+            c: 'They are different, but they are interchangable.',
+            d: 'They are just different versions of the same thing.'
+        },
+        correctAnswer: 'b'
+        },
+
+        {
+        question: "How do you call a function in JavaSctipt?"
+        answers: {
+            a: 'You ask for its phone number, and hope it gies it to you.',
+            b: 'You write function.functionname().',
+            c: 'Wite functionname().',
+            d: 'Use the call() function.'
+        },
+        correctAnswer: 'c'
+        },
+      {
+        question: "How would you get an element by its id in JavaScript?"
+        answers: {
+            a: 'myElement = document.getElementById("id01");',
+            b: 'myElement = $("#id01");.',
+            c: 'myElement = document.retrieveElementbyId("id01");.',
+            d: 'myElement = &("#id01");.'
+        },
+        correctAnswer: 'a'
+        },
+
+    {
+        question: "What is the syntax for preventing a page from refreshing after a function runs?"
+        answers: {
+            a: 'function.preventRefresh();.',
+            b: 'function.preventDefault();.',
+            c: 'event.preventRefresh();.',
+            d: 'event.preventDefault();.'
+        },
+        correctAnswer: 'd'
+        },
+]
